6674895f190d5d8c2b973eb909835543e7906955
Lance Haig <lnhaig@gmail.com>
2019-05-21 22:04:11 +0200

    Update Outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "linux_vm_public_ip" {
-  value = "${module.vault-server.public_ip_address}"
-}
+# output "linux_vm_public_ip" {
+#   value = "${module.vault-server.public_ip_address}"
+# }
 
-output "windows_client_public_ip" {
-value = "${module.windows-client.public_ip_address}"
-}
+# output "windows_client_public_ip" {
+# value = "${module.windows-client.public_ip_address}"
+# }
