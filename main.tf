1a0ec050185879234895046b33d3236de6405914
joestack <joern@joern-online.de>
2019-05-22 09:06:53 +0200

    adding vars

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
+variable "resource_group_name" {}
+variable "environment_tag" {}
+variable "location" {}
+
+
+
 module "network" {
   source  = "app.terraform.io/JoeStack/network/azurerm"
   version = "2.0.0"
-  location            = "westus"
-  resource_group_name = "js-vault-demo"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group_name}"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
   tags                = {
-                          environment = "joestack-vault"
+                          environment = "${var.environment_tag}"
                         }
 }
