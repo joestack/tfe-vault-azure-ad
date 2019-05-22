07775f9de016bdc5ccd62cbff4e1d9fa6b5735bf
joestack <joern@joern-online.de>
2019-05-22 08:51:34 +0200

    network test

--- main.tf
+++ main.tf
@@ -1,41 +1,13 @@
-terraform {
-  required_version = ">= 0.11.1"
-}
-
-variable "location" {
-  description = "Azure location in which to create resources"
-  default = "East US"
-}
-
-variable "windows_dns_prefix" {
-  description = "DNS prefix to add to to public IP address for Windows VM"
-}
-
-variable "admin_password" {
-  description = "admin password for Windows VM"
-}
-
-module "windowsserver" {
-  source              = "Azure/compute/azurerm"
-  version             = "1.1.5"
-  location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
-  vm_hostname         = "pwc-ptfe"
-  admin_password      = "${var.admin_password}"
-  vm_os_simple        = "WindowsServer"
-  public_ip_dns       = ["${var.windows_dns_prefix}"]
-  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-}
-
 module "network" {
-  source              = "Azure/network/azurerm"
-  version             = "1.1.1"
-  location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
-  allow_ssh_traffic   = true
-}
+  source  = "app.terraform.io/JoeStack/network/azurerm"
+  version = "2.0.0"
+  location            = "westus"
+  resource_group_name = "js-vault-demo"
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
-output "windows_vm_public_name"{
-  value = "${module.windowsserver.public_ip_dns_name}"
+  tags                = {
+                          environment = "joestack-vault"
+                        }
 }
-
