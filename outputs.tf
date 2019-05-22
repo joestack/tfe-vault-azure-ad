3d6f80f1f2a3297fe88e947eff6da67aacedb2c1
Lance Haig <lnhaig@gmail.com>
2019-05-22 15:55:09 +0200

    Fix Outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "linux_vm_public_ip" {
-  value = "${module.vault-server.public_ip_address}"
-}
+# output "linux_vm_public_ip" {
+#   value = "${module.vault-server.public_ip_address}"
+# }
 
-output "windows_client_public_ip" {
-value = "${module.windows-client.public_ip_address}"
+# output "windows_client_public_ip" {
+# value = "${module.windows-client.public_ip_address}"
+# }
+
+output "AD_vm_private_ip" {
+  value = "${module.active-directory-domain.private_ip_address}"
 }
